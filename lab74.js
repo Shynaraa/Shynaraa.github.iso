793ab80a6add0753eeb293964340804c3f1020b3
Shynar Toktar <180103079@stu.sdu.edu.kz>
2019-12-13 17:16:29 +0600

    first

---
+++ lab74.js
@@ -0,0 +1,138 @@
+function m1(){
+	var x = document.getElementById('counter1');
+	var y = document.getElementById('sum').value;
+	if(x.value>0){
+		x.value--;
+		document.getElementById('sum').value = Number(y) - Number(700);
+	}
+}
+
+function a1(){
+	var x = document.getElementById('counter1');
+	var y = document.getElementById('sum').value;
+	x.value++;
+	document.getElementById('sum').value = Number(700) + Number(y);
+}
+
+function m2(){
+	var x = document.getElementById('counter2');
+	var y = document.getElementById('sum').value;
+	if(x.value>0){
+		x.value--;
+		document.getElementById('sum').value = Number(y) - Number(800);
+	}
+}
+
+function a2(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter2');
+	x.value++;
+	document.getElementById('sum').value = Number(800) + Number(y);
+
+}
+
+function m3(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter3');
+	if(x.value>0){
+		x.value--;
+		document.getElementById('sum').value = Number(y) - Number(900);
+	}
+}
+
+function a3(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter3');
+	x.value++;
+	document.getElementById('sum').value = Number(900) + Number(y);
+}
+
+function m4(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter4');
+	if(x.value>0){
+		x.value--;
+		document.getElementById('sum').value = Number(y) - Number(1000);
+
+	}
+}
+
+function a4(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter4');
+	x.value++;
+	document.getElementById('sum').value = Number(1000) + Number(y);
+
+}
+
+function m5(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter5');
+	if(x.value>0){
+		x.value--;
+		document.getElementById('sum').value = Number(y) - Number(1100);
+
+	}
+}
+
+function a5(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter5');
+	x.value++;
+	document.getElementById('sum').value = Number(1100) + Number(y);
+
+}
+
+function m6(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter6');
+	if(x.value>0){
+		x.value--;
+		document.getElementById('sum').value = Number(y) - Number(1200);
+
+	}
+}
+
+function a6(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter6');
+	x.value++;
+	document.getElementById('sum').value = Number(1200) + Number(y);
+
+}
+
+function m7(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter7');
+	if(x.value>0){
+		x.value--;
+		document.getElementById('sum').value = Number(y) - Number(1300);
+
+	}
+}
+
+function a7(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter7');
+	x.value++;
+	document.getElementById('sum').value = Number(1300) + Number(y);
+
+}
+
+function m8(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter8');
+	if(x.value>0){
+		x.value--;
+		document.getElementById('sum').value = Number(y) - Number(1400);
+
+	}
+}
+
+function a8(){
+	var y = document.getElementById('sum').value;
+	var x = document.getElementById('counter8');
+	x.value++;
+	document.getElementById('sum').value = Number(1400) + Number(y);
+
+}
